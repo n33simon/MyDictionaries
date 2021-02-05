a7490365bc32880a2695f15ff67cfc884588e9e6
Nicole Simon <33nicole.simon@gmail.com>
2021-02-05 16:37:29 -0600

    Edit 1

--- Dictionary Exercise.py
+++ Dictionary Exercise.py
@@ -1,7 +1,15 @@
-filename = "text.txt"
+def count_words(filename):
+    """Count the approximate number of words in a file."""
+    try:
+        with open(filename, encoding="utf-8") as f:
+            contents = f.read()
+    except FileNotFoundError:
+        pass
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words.")
 
-try:
-    with open(filename) as f:
-        contents = f.read()
-        print(contents)
- 
+
+filename = "text.txt"
+count_words(filename)
