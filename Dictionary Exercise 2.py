0955491e9c88a62dedebd2065c682c8c7002988c
Nicole Simon <33nicole.simon@gmail.com>
2021-02-06 22:57:36 -0600

    Revert to working version

--- Dictionary Exercise 2.py
+++ Dictionary Exercise 2.py
@@ -8,19 +8,19 @@ def main():
     # create lines
     lines = create_lines()
 
+    # prompt user
+    winner_record = int(
+        input("What year would you like to know the World Series Winner? ")
+    )
+
     # create dictionary 1
-    records = dict_1(lines, winner_record)
+    records = dict_1(lines)
 
     # create dictionary 2
-    series = dict_2(lines, index, winner_record)
+    series = dict_2(lines, index)
 
     # get Series and Record finding
     output()
-
-    # prompt user
-    winner_record = input("Year? ")
-    input = int(winner_record)
-    print({winner_record})
 
 
 def create_lines():
@@ -31,27 +31,26 @@ def create_lines():
     return lines
 
 
-def dict_1(lines, winner_record):
+def dict_1(lines):
     for line in lines:
         if line not in roster:
             roster[line] = lines.count(line)
-    # print(roster)
+    print(roster)
     return roster
 
 
-def dict_2(lines, index, winner_record):
+def dict_2(lines, index):
     for line in lines:
         if index not in series_record:
             series_record[index] = line
             index += 1
     return series_record
-    # print(series_record)
+    print(series_record)
 
 
 def output(winner_record, roster, series_record):
-    for count in range(winner_record):
-        winner = series_record.get("f{winner_record}")
-        record = roster.get("f{winner}")
+    winner = series_record.get(winner_record)
+    record = roster.get(winner)
     return winner
     return record
 
