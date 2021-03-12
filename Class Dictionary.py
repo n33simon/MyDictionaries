fe124a3440a30b7258e682485c0905eed4e0a235
Nicole Simon <33nicole.simon@gmail.com>
2021-03-11 21:42:55 -0600

    Final Commit

---
+++ Class Dictionary.py
@@ -0,0 +1,18 @@
+crn_dictionary = {}
+
+def main():
+
+    #values
+    crn_1 = str("125468")
+    crn_2 = str("985474")
+
+    #appending
+    for crn in CourseClass:
+        if crn not in crn_dictionary:
+            crn_dictionary[crn] = cap
+
+    print(crn_dictionary)
+
+
+
+main()
